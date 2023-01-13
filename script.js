e1db729864a50896f2364d5ff75548f64ac9680e
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-01-13 17:45:04 +0530

    optimal solution

--- script.js
+++ script.js
@@ -1,37 +1,28 @@
 function threeSum(arr, target) {
   //your code here
-	arr.sort(function(a,b){return a-b});
-	let n=arr.length;
-	let least_sum=arr[0]+arr[1]+arr[2];
-	let least_diff=target-least_sum;
-	least_diff=(least_diff>=0)?least_diff:-least_diff;
-	let i=0,j=1,k=2;
-	while(i<n && j<n && k<n){
-		// console.log(arr[i], arr[j], arr[k]);
-		let curr_sum=arr[i]+arr[j]+arr[k];
-		let curr_diff=target-curr_sum;
-		curr_diff=(curr_diff>=0)?curr_diff:-curr_diff;
-		if(curr_diff<least_diff){
-			least_diff=curr_diff;
-			least_sum=curr_sum;
-		}
-		if(curr_sum==target){
-			return least_sum;
-		}else{
-			if(k<n-1){
-				k++;
-			}else if(j<n-2){
-				j++;
-				k=j+1;
-			}else{
-				i++;
-				j=i+1;
-				k=j+1;
-			}
-		}
-		// console.log(least_sum);
-	}
-	return least_sum;
+  arr.sort(function (a, b) {
+    return a - b;
+  });
+  let i, j, k;
+  let nearest_target = 0,
+    least_diff = Number.MAX_VALUE;
+  for (i = 0; i < arr.length - 2; i++) {
+    j = i + 1;
+    k = arr.length - 1;
+    while (j < k) {
+      let sum = Number(arr[i]) + Number(arr[j]) + Number(arr[k]);
+      let diff = Math.abs(sum - target);
+      if (diff < least_diff) {
+        least_diff = diff;
+        nearest_target = sum;
+      }
+      if (sum == target) return nearest_target;
+      else if (sum > target) k--;
+      else j++;
+    }
+  }
+  return nearest_target;
 }
-
+// let arr=['1','2','-4','-1'],target=1;
+// console.log(threeSum(arr, target));
 module.exports = threeSum;
